06bb15dda3f60378d438e73691a65a9b62c422fb
ZhaonanLi <zhaonanproject@gmail.com>
2016-08-21 12:22:10 -0700

    Init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# iOS-Play-Video
--- ios-play-video/ViewController.swift
+++ ios-play-video/ViewController.swift
@@ -7,12 +7,44 @@
 //
 
 import UIKit
+import Photos
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    @IBOutlet weak var videoView: UIView!
+    @IBOutlet weak var playVideoBtn: UIButton!
+    @IBOutlet weak var selectVideoBtn: UIButton!
+    @IBOutlet weak var pauseVideoBtn: UIButton!
+    
+    var videoUrl: NSURL?
+    
+    lazy var videoPickerController: UIImagePickerController = {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self;
+        pickerController.sourceType = .PhotoLibrary
+        pickerController.mediaTypes = ["public.movie"]
+        // pickerController.mediaTypes = ["public.image", "public.movie"]
+        return pickerController
+    }()
 
+    lazy var avPlayer: AVPlayer = {
+        let avPlayer = AVPlayer()
+        return avPlayer
+    }()
+    
+    lazy var avPlayerLayer: AVPlayerLayer = {
+        let avPlayerLayer = AVPlayerLayer(player: self.avPlayer)
+        return avPlayerLayer
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        self.avPlayerLayer.backgroundColor = UIColor.blackColor().CGColor
+        self.avPlayerLayer.frame = CGRectMake(0, 0, self.videoView.frame.width, self.videoView.frame.height)
+        self.videoView.layer.insertSublayer(self.avPlayerLayer, atIndex: 0)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +53,37 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func playVideo(sender: AnyObject) {
+        if self.videoUrl == nil {
+            print("ERROR:videoUrl is nil")
+            return
+        }
+        
+        print("DEBUG:videoUrl=\(self.videoUrl)")
+        
+        let avPlayerItem: AVPlayerItem = AVPlayerItem(URL: self.videoUrl!)
+        self.avPlayer.replaceCurrentItemWithPlayerItem(avPlayerItem)
+        self.avPlayer.play()
+    }
+    
+    @IBAction func selectVideo(sender: AnyObject) {
+        presentViewController(self.videoPickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func pauseVideo(sender: AnyObject) {
+        self.avPlayer.pause()
+    }
+    
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        self.videoUrl = info["UIImagePickerControllerReferenceURL"] as? NSURL
+        
+        print(self.videoUrl)
+        self.videoPickerController.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
 
